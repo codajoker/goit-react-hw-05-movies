7b33fa4c6f47b7dce7e0fdec5b99dee2a0a30b13
codajoker <84983434+codajoker@users.noreply.github.com>
2022-02-08 12:30:35 +0000

    Deploying to gh-pages from @ codajoker/goit-react-hw-05-movies@20d696a5a82859c91278ddc0f69eddb537e8d9eb 🚀

---
+++ static/js/716.f6b2007b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[716],{3716:function(e,t,a){a.r(t),t.default={nav:"NavigationStyle_nav__wpnq4",link:"NavigationStyle_link__PXhHD",activeLink:"NavigationStyle_activeLink__r7TC0"}}}]);
+//# sourceMappingURL=716.f6b2007b.chunk.js.map
